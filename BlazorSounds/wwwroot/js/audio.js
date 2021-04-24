5fe2c503fe7438824f0328c6b83e71bc64c546b7
Benjamin Kent <benjaminfranklinkent@gmail.com>
2021-04-24 16:47:40 -0700

    Add project files.

---
+++ BlazorSounds/wwwroot/js/audio.js
@@ -0,0 +1,11 @@
+﻿window.PlayAudio = (elementName) => {
+    document.getElementById(elementName).play();
+    // alert("playing")
+}
+window.PauseAudio = (elementName) => {
+    document.getElementById(elementName).pause();
+    
+}
+window.ReloadAudio = (elementName) => {
+    document.getElementById(elementName).load();
+}
