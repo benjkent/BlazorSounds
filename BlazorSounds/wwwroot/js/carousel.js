1ec7e0b345934b0161b674b2e0cc154c8582429b
Benjamin Kent <benjaminfranklinkent@gmail.com>
2021-05-10 19:07:35 -0700

    add dymaic pages and carousel comp

---
+++ BlazorSounds/wwwroot/js/carousel.js
@@ -0,0 +1,10 @@
+﻿window.initializeCarousel = () => {
+    $('#carouselExampleIndicators').carousel({ interval: 4000 });
+
+    //see step 2 to understand these news id's:
+    $('#carouselExampleIndicators-prev').click(
+        () => $('#carouselExampleIndicators').carousel('prev'));
+    $('#carouselExampleIndicators-next').click(
+        () => $('#carouselExampleIndicators').carousel('next'));
+
+}
