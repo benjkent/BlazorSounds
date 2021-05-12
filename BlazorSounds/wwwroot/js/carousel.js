ecdedfec7a077fb2c94a7bfd18f5428486492d72
Benjamin Kent <benjaminfranklinkent@gmail.com>
2021-05-11 22:07:00 -0700

    add multi slider

--- BlazorSounds/wwwroot/js/carousel.js
+++ BlazorSounds/wwwroot/js/carousel.js
@@ -1,10 +1,12 @@
 ﻿window.initializeCarousel = () => {
     $('#carouselExampleIndicators').carousel({ interval: 4000 });
 
-    //see step 2 to understand these news id's:
+    //see step 2 to understand these new id's:
     $('#carouselExampleIndicators-prev').click(
         () => $('#carouselExampleIndicators').carousel('prev'));
     $('#carouselExampleIndicators-next').click(
         () => $('#carouselExampleIndicators').carousel('next'));
 
 }
+
+
