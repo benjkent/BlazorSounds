6924cfff53bfefa019054bf0746f092274f0b988
Benjamin Kent <benjaminfranklinkent@gmail.com>
2022-04-19 18:20:46 -0700

    Update project to .net 6

--- BlazorSounds/Program.cs
+++ BlazorSounds/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,6 +18,7 @@ namespace BlazorSounds
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddFileReaderService(options => options.UseWasmSharedBuffer = true);
