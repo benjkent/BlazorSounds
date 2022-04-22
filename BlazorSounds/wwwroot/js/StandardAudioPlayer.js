c035f6e1e7633647754b87930930f7ec45c13da6
Benjamin Kent <benjaminfranklinkent@gmail.com>
2022-04-21 19:59:12 -0700

    Add StandardAudioPlayer component

---
+++ BlazorSounds/wwwroot/js/StandardAudioPlayer.js
@@ -0,0 +1,10 @@
+﻿
+export function PlayAudio(elementName) {
+    document.getElementById(elementName).play();
+}
+export function PauseAudio(elementName) {
+    document.getElementById(elementName).pause();
+}
+export function ReloadAudio(element) {
+    document.getElementById(elementName).load();
+}
